0dcec2cc73e5bf25d5bc71ea7379737f8f662fb3
Mohd Jibraeel <mohdjibrail424@gmail.com>
2026-01-29 19:14:06 +0530

    create server

---
+++ app.js
@@ -0,0 +1,20 @@
+//External Modules
+const dotenv= require('dotenv');
+dotenv.config();
+const express=require('express');
+const cors=require('cors');
+
+const app=express();
+
+app.use(cors());
+
+
+app.get('/',(req,res,next)=>{
+  console.log('Hello from Uber');
+  res.send('Hello from Uber');
+})
+
+
+
+
+module.exports=app;
