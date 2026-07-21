4358cdd3a205aed215818d46507a536f85023d7e
Mohd Jibraeel <mohdjibrail424@gmail.com>
2026-07-21 17:04:19 +0530

    Hardcoded URL updated

--- Frontend/src/Pages/UserLogin.jsx
+++ Frontend/src/Pages/UserLogin.jsx
@@ -18,7 +18,7 @@ const UserLogin = () => {
       email: email,
       password: password
     }
-    const response= await axios.post('http://localhost:3000/users/login',user);
+    const response= await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`,user);
 
     if(response.status===200){
       const data=response.data;
